36a7a57ce1475e072d9f21f5bee7a818cc854f16
danieldkato <lab@hs05bruno8.cpmc.columbia.edu>
2017-07-19 11:25:52 -0400

    Edit documentation for getSHA1.m

--- getSHA1.m
+++ getSHA1.m
@@ -1,23 +1,45 @@
-function SHA1 = getSHA1()
+function SHA1 = getSHA1(varargin)
 
 % DOCUMENTATION TABLE OF CONTENTS
 % I. OVERVIEW
-% II. REQUIREMENTS
-% III. INPUTS
-% IV. OUTPUTS
+% II. SYNTAX
+% III. REQUIREMENTS
+% IV. INPUTS
+% V. OUTPUTS
+
 
 %% I. OVERVIEW
-% This function returns the SHA1 of the most recent git commit of the
-% calling script.
+% This function returns the SHA1 hash of the most recent git commit of a
+% given file. If no file is specified in the input, it returns the SHA1
+% hash of the most recent git commit of the calling script.
 
 
-%% REQUIREMENTS
+%% II. SYNTAX
+% SHA1 = getSHA1()
+% SHA1 = getSHA1(path)
+
+
+%% III. REQUIREMENTS
 % 1) git, available at https://git-scm.com/.
-% 2) Operating system configured to recognize `git` as a command. 
+% 2) Operating system configured to recognize `git` as a command. If `git`
+% is not automatically recognized as a command after installing on Windows,
+% add it to the Path environment variable as follows:
+%   a) Under the start menu, right click on 'My Computer' and naivgate to 'Properties'.
+%   b) Navigate to the 'Advanced' tab and click on the 'Environment Variables' button.
+%   c) In the 'System variables' listbox, select 'Path' and click on the 'Edit' button.
+%   d) In the 'Variable value' text field, add the following text:
+%      
+%      <path\to>\Git\bin\git.exe;<path\to>\Git\cmd;
+%       
+%      where <path\to> it the absolute path of the directory where the top-level Git folder is located
 
 
-%% INPUTS
-% None.
+%% IV. INPUTS
+% 1) path - path to a file of which the SHA hash of the most recent git
+% commit will be returned.
+
+% If no input argument is provided, this function will return the SHA1 hash
+% of the most recent git commit of the calling function. 
 
 
 %% OUTPUTS
@@ -32,22 +54,29 @@ function SHA1 = getSHA1()
 
 % last updated DDK 2017-07-15
 
+
 %%
-    % Get the complete file name of the calling fucntion
-    ST = dbstack('-completenames');
-    [pathstr, filename, ext] = fileparts(ST(2).file);
+    % Get the name of the function to get the commit for:
+    if nargin<1
+        ST = dbstack('-completenames');
+        path = ST(2).file;
+    else
+        path = varargin{1};
+    end
+    
+    [pathstr, filename, ext] = fileparts(path);
     
     % cd to the direcory of the calling fucntion:
     old = cd(pathstr);
     
-    % get its SHA1
+    % Get its SHA1:
     [status, SHA1] = system(strcat(['git log -n 1 --pretty=format:%H -- ', filename, ext]));
     
     if isempty(SHA1)
         SHA1 = 'No commits found for current script.';
     end
     
-    % return to the previous working directory
+    % Return to the previous working directory
     cd(old);
     
 end
